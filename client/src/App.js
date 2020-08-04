5dafc8232dcce4411a21ec8b9590caacd9b55214
i11f2jz8e7 <dev.i11f2jz8e7@gmail.com>
2020-08-04 14:19:33 -0700

    init

---
+++ client/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Otherpage from './Otherpage';
+import Fib from './Fib';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Just a test app...</h1>
+            <Link to="/">Home</Link>
+            <Link to="/Otherpage">Other Page</Link>
+          </header>
+          <div>
+            <Route exact path="/" component={Fib}/>
+            <Route path="/Otherpage" component={Otherpage}/>
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ client/src/Otherpage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+export default () => {
+    return(
+        <div>
+            A generic test page--nothing here.
+            <Link to="/"> Back Home </Link>
+        </div>
+    );
+};
